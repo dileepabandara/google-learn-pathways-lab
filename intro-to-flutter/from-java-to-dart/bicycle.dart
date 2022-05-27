cf7097c4823bb2e1bea3c3f19aa54a8805490ded
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-05-27 21:29:49 +0530

    Add a read-only variable

--- intro-to-flutter/from-java-to-dart/bicycle.dart
+++ intro-to-flutter/from-java-to-dart/bicycle.dart
@@ -1,20 +1,33 @@
 class Bicycle {
   int cadence;
-  int speed;
+  // int speed;
+  int _speed = 0;
+  int get speed => _speed;
   int gear;
 
   // Bicycle(this.cadence, this.speed, this.gear);
 
-  Bicycle(int cadence, int speed, int gear)
-      : this.cadence = cadence,
-        this.speed = speed,
-        this.gear = gear;
+  // Bicycle(int cadence, int speed, int gear)
+  //     : this.cadence = cadence,
+  //       this.speed = speed,
+  //       this.gear = gear;
+
+  Bicycle(this.cadence, this.gear);
+
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
 
   @override
-  String toString() => 'Bicycle: $speed mph';
+  String toString() => 'Bicycle: $_speed mph';
 }
 
 void main() {
-  var bike = new Bicycle(2, 0, 1);
+  // var bike = new Bicycle(2, 0, 1);
+  var bike = Bicycle(2, 1);
   print(bike);
 }
