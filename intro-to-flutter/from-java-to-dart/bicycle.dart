88a023d638cf42c6c2777a15dc3ffe8652f03aa7
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-05-27 21:26:02 +0530

    Define a Bicycle constructor

--- intro-to-flutter/from-java-to-dart/bicycle.dart
+++ intro-to-flutter/from-java-to-dart/bicycle.dart
@@ -2,7 +2,13 @@ class Bicycle {
   int cadence;
   int speed;
   int gear;
+
+  // Bicycle(this.cadence, this.speed, this.gear);
+
+  Bicycle(int cadence, int speed, int gear)
+      : this.cadence = cadence,
+        this.speed = speed,
+        this.gear = gear;
 }
 
-void main() {
-}
+void main() {}
