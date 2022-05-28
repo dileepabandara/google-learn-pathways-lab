728dc960a138e42d18f15e908fcc12a58919fbb4
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-05-28 12:17:15 +0530

    Option 1: Create a top-level function

--- intro-to-flutter/from-java-to-dart/shapes.dart
+++ intro-to-flutter/from-java-to-dart/shapes.dart
@@ -16,9 +16,17 @@ class Square implements Shape {
   num get area => pow(side, 2);
 }
 
+Shape shapeFactory(String type) {
+  if (type == 'circle') return Circle(2);
+  if (type == 'square') return Square(2);
+  throw 'Can\'t create $type.';
+}
+
 main() {
-  final circle = Circle(2);
-  final square = Square(2);
+  // final circle = Circle(2);
+  // final square = Square(2);
+  final circle = shapeFactory('circle');
+  final square = shapeFactory('square');
   print(circle.area);
   print(square.area);
 }
