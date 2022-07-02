25cf9f6147d375aed6881992478154454058fdcc
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-07-02 12:05:05 +0530

    03-08 -> Exercises

---
+++ android-basics-compose-unit-1-pathway-1/03-your-first-program-in-kotlin/src/main/kotlin/03_exercises.kt
@@ -0,0 +1,17 @@
+fun main() {
+    println("1")
+    println("2")
+    println("3")
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+    println("Tuesday")
+    println("Thursday")
+    println("Wednesday")
+    println("Friday")
+    println("Monday")
+    println("Tomorrow is rainy")
+    println("There is a chance of snow")
+    println("Cloudy"); println("Partly Cloudy"); println("Windy")
+    println("How's the weather today?")
+}
