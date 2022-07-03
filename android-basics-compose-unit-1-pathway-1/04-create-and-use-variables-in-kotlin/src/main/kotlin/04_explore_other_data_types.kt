89c83cf473d09772c4986b3579f7bfb3ae7986aa
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-07-03 12:34:25 +0530

    04-05 -> Explore other data types

---
+++ android-basics-compose-unit-1-pathway-1/04-create-and-use-variables-in-kotlin/src/main/kotlin/04_explore_other_data_types.kt
@@ -0,0 +1,22 @@
+fun main() {
+//    val trip1: Double = 3.20
+//    val trip2: Double = 4.10
+//    val trip3: Double = 1.72
+//    var totalTripLength: Double = 0.0
+    val trip1 = 3.20
+    val trip2 = 4.10
+    val trip3 = 1.72
+    var totalTripLength = 0.0
+    totalTripLength = trip1 + trip2 + trip3
+    println("$totalTripLength miles left to destination")
+
+    val nextMeeting = "Next meeting is: "
+    val date = "January 1"
+    val reminder = nextMeeting + date + " at work"
+    println(reminder)
+
+    println("Say \"hello\"")
+
+    val notificationsEnabled: Boolean = false
+    println("Are notifications enabled? $notificationsEnabled")
+}
